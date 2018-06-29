5f479a633f9b0d5b3328fc3d1d59611155724c58
Anu Tomy <anuatomy@gmail.com>
2018-06-29 18:21:14 -0400

    Add Comments

--- src/test/FirstSeleniumTest.java
+++ src/test/FirstSeleniumTest.java
@@ -8,11 +8,15 @@ public class FirstSeleniumTest {
 	
 	public static void main(String[] args) {
 		
+		//Specify the driver type and its location
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\tomya\\eclipse-workspace\\geckodriver.exe");
 		
+		//Create an object of the webdriver
 		WebDriver driver = new FirefoxDriver();		
 			
+		//navigation
 		driver.get("http://www.seleniumhq.org/");
+		
 		
 		driver.quit();
 	}

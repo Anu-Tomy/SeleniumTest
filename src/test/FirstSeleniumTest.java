e4c1c182ea28253ea347717f54fdf884bd09c0c0
Anu Tomy <anuatomy@gmail.com>
2018-06-29 20:15:07 -0400

    Set up relative path

--- src/test/FirstSeleniumTest.java
+++ src/test/FirstSeleniumTest.java
@@ -9,14 +9,18 @@ public class FirstSeleniumTest {
 	public static void main(String[] args) {
 		
 		//Specify the driver type and its location
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\tomya\\eclipse-workspace\\geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\tomya\\eclipse-workspace\\geckodriver.exe");
+		
+		//set up relative path 
+		String projectLocation = System.getProperty("user.dir"); //user working directory
+		
+		System.setProperty("webdriver.gecko.driver", projectLocation + "\\lib\\geckodriver\\geckodriver.exe");
 		
 		//Create an object of the webdriver
 		WebDriver driver = new FirefoxDriver();		
 			
 		//navigation
 		driver.get("http://www.seleniumhq.org/");
-		
 		
 		driver.quit();
 	}

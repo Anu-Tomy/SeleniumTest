f68755926242f2208952c626ba02aaec864a8ff0
Anu Tomy <anuatomy@gmail.com>
2018-07-02 14:25:18 -0400

    Make code Modular

---
+++ src/test/SeleniumTest_modular.java
@@ -0,0 +1,56 @@
+package test;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class SeleniumTest_modular {
+	
+	static String browser;
+	static WebDriver driver;
+	
+
+	public static void main(String[] args) throws Exception {
+		
+		setBrowser();
+		setBrowserConfig();
+		runTest();		
+		
+	}
+	
+	public static void setBrowser() {	 
+		
+		browser = "Firefox";
+	    
+	}
+		
+	public static void setBrowserConfig() {
+		
+		String projectLocation = System.getProperty("user.dir");
+		
+		if (browser.contains("Firefox")) {			
+			System.setProperty("webdriver.gecko.driver", projectLocation + "\\lib\\geckodriver\\geckodriver.exe");
+			driver = new FirefoxDriver();			
+		}
+		
+		else if(browser.contains("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectLocation + "\\lib\\chromedriver\\chromedriver.exe");
+			driver = new org.openqa.selenium.chrome.ChromeDriver();
+		}
+	}
+	
+	public static void runTest() throws InterruptedException {
+		
+		driver.get("http://www.seleniumhq.org/");
+		
+		//wait
+		Thread.sleep(5000);
+		
+		driver.close();
+		
+	}
+
+}
+	
+	
